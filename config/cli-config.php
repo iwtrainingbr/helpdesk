ce3e0b73c5958648e031b359cf13ed4ba9c0df61
Alessandro Feitoza <eu@alessandrofeitoza.eu>
2019-12-21 14:28:01 -0300

    Adicionando arquivo com as constantes de conexão no cli-config.php

--- config/cli-config.php
+++ config/cli-config.php
@@ -5,6 +5,8 @@ use Root\Adapter\Connection;
 
 include_once dirname(__DIR__).'/vendor/autoload.php';
 
+include_once 'database.php';
+
 $entityManager = Connection::getEntityManager();
 
 return ConsoleRunner::createHelperSet($entityManager);
